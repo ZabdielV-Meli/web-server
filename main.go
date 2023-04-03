8aaa62b8751cbc3d8fe8b95eba3c634deb52aab5
ZabdielV-Meli <zabdiel.garduno@mercadolibre.com.mx>
2023-04-02 23:12:40 -0600

    metodo POST agregar producto

--- main.go
+++ main.go
@@ -6,19 +6,20 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"regexp"
 	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
 
 type Producto struct {
-	ID           int     `json:"id" binding:"required"`
-	Name         string  `json:"name" binding:"required"`
-	Quantity     int     `json:"quantity" binding:"required"`
-	Code_value   int     `json:"code_value" binding:"required"`
-	Is_published bool    `json:"is_published" binding:"required"`
-	Expiration   string  `json:"expiration" binding:"required"`
-	Price        float64 `json:"price" binding:"required"`
+	ID           int     `json:"id"`
+	Name         string  `json:"name"`
+	Quantity     int     `json:"quantity"`
+	Code_value   int     `json:"code_value" `
+	Is_published bool    `json:"is_published"`
+	Expiration   string  `json:"expiration" `
+	Price        float64 `json:"price"`
 }
 
 func buscarPorId(id int, sliceProdcuctos *[]Producto) (Producto, error) {
@@ -32,24 +33,90 @@ func buscarPorId(id int, sliceProdcuctos *[]Producto) (Producto, error) {
 	return Producto{}, errors.New("ID no encontrado")
 }
 
-func filtrarSlice(costo float64, sliceProdcuctos *[]Producto) []Producto {
+func filtrarSlice(c *gin.Context) {
+
+	//castear de string a int
+	priceGt, _ := strconv.ParseFloat(c.Query("priceGt"), 64)
 
 	sliceQuery := []Producto{}
 
-	for _, valor := range *sliceProdcuctos {
-		if valor.Price > costo {
+	for _, valor := range sliceProdcuctos {
+		if valor.Price > priceGt {
 			sliceQuery = append(sliceQuery, valor)
 		}
 	}
 
-	return sliceQuery
+	c.JSON(200, sliceQuery)
 }
 
-func main() {
+func buscarProducto(c *gin.Context) {
 
-	//Slice de productos
-	var sliceProdcuctos []Producto
+	//castear de string a int
+	id, _ := strconv.Atoi(c.Param("id"))
 
+	//Buscar por id
+	producto, err := buscarPorId(id, &sliceProdcuctos)
+	if err != nil {
+		c.JSON(404, gin.H{
+			"message": "Id no existe",
+		})
+	} else {
+		c.JSON(200, producto)
+	}
+
+}
+
+func guardar() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		var req Producto
+		if err := ctx.ShouldBindJSON(&req); err != nil {
+			ctx.JSON(404, gin.H{
+				"error": "parametros no validos",
+			})
+			fmt.Println(err)
+			return
+		}
+		//parametros vacios
+		if req.Name == "" || req.Price == 0.0 || req.Code_value == 0 || req.Quantity == 0 || req.Price == 0 || req.Expiration == "" {
+			ctx.JSON(404, gin.H{
+				"error": "parametro/s vacios",
+			})
+			return
+		}
+		//Code value unico
+		for _, valor := range sliceProdcuctos {
+			if req.Code_value == valor.Code_value {
+				ctx.JSON(404, gin.H{
+					"error": "code_value no es unico",
+				})
+				return
+			}
+		}
+
+		//validar fecha
+		// parse string date to golang time
+		re := regexp.MustCompile("^[0-3]?[0-9][/][0-3]?[0-9][/]([0-9]{2})?[0-9]{2}$")
+		//_, err := time.Parse("01/02/2006", req.Expiration)
+		if !re.MatchString(req.Expiration) {
+			ctx.JSON(404, gin.H{
+				"error": "fecha en formato incorrecto",
+			})
+			return
+		}
+		req.ID = len(sliceProdcuctos) + 1
+		sliceProdcuctos = append(sliceProdcuctos, req)
+		ctx.JSON(200, gin.H{
+			"ok":       "producto añadido correctamente",
+			"producto": req,
+		})
+	}
+}
+
+// Slice de productos
+var sliceProdcuctos []Producto
+
+// Inicializa los productos a un slice
+func getData(sliceProducts *[]Producto) {
 	//leer el archivo JSON
 	arrayBites, err := os.ReadFile("products.json")
 
@@ -63,13 +130,14 @@ func main() {
 		}
 	}
 
-	/*
-		 	fmt.Println(len(sliceProdcuctos))
-			fmt.Println(sliceProdcuctos[0].Price)
-	*/
+}
+func main() {
 
 	// Crea un router con gin
 	router := gin.Default()
+
+	//Get data
+	getData(&sliceProdcuctos)
 
 	//Ruta /ping
 	router.GET("/ping", func(c *gin.Context) {
@@ -83,34 +151,17 @@ func main() {
 	})
 
 	//Ruta /products/:id
-	router.GET("/products/:id", func(c *gin.Context) {
+	router.GET("/products/:id", buscarProducto)
 
-		//castear de string a int
-		id, _ := strconv.Atoi(c.Param("id"))
-
-		//Buscar por id
-		producto, err := buscarPorId(id, &sliceProdcuctos)
-		if err != nil {
-			c.JSON(404, gin.H{
-				"message": "Id no existe",
-			})
-		} else {
-			c.JSON(200, producto)
-		}
-
-	})
+	pr := router.Group("/products")
 
 	//Ruta /products/search
-	router.GET("/products/", func(c *gin.Context) {
+	pr.GET("/", filtrarSlice)
+	//Guardar producto
+	pr.POST("/", guardar())
 
-		//castear de string a int
-		priceGt, _ := strconv.ParseFloat(c.Query("priceGt"), 64)
-
-		sliceQuery := filtrarSlice(priceGt, &sliceProdcuctos)
-
-		c.JSON(200, sliceQuery)
-
-	})
+	//Ruta /products/search
+	//router.GET("/products", filtrarSlice)
 
 	// Corremos nuestro servidor sobre el puerto 8080
 	router.Run()
